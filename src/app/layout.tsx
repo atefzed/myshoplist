ca5b68f24abeb21c1bd9fe9da0c130e9d31125e0
atefzed <atefzed@gmail.com>
2025-03-28 02:12:29 +0100

    fix list page params type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -29,7 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+      <Suspense>
         {children}
+      </Suspense>
       </body>
     </html>
   );
--- src/app/list/[listId]/page.tsx
+++ src/app/list/[listId]/page.tsx
@@ -1,6 +1,6 @@
 import ShoppingList from '@/components/shop-list'
 
-export default async function ListPage({ params }: { params: { listId: string } }) {
+export default async function ListPage({ params }: { params: Promise<{ listId: string }> }) {
 
   const { listId } = await params;
   if (!listId) return <div>List not found</div>
